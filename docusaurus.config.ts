d21803bcc655bb73d356a9a07bb7e6fea1fe2002
Robin Opletal <me@robinopletal.com>
2023-11-08 11:48:12 +0100

    feat(header): change lame source code to cool github svg

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,8 +69,9 @@ const config: Config = {
         { to: "/blog", label: "Blog", position: "left" },
         {
           href: "https://github.com/oskoperator/osko",
-          label: "Source code",
           position: "right",
+          className: "header-github-link",
+          "aria-label": "GitHub repository",
         },
       ],
     },
